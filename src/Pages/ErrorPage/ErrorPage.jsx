3916a13398839d093d319d03f7744d830f6e7e37
nirjhor saha <nirjhor.badhan25@gmail.com>
2023-05-03 01:55:59 +0600

    implement login page

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ErrorPage = () => {
     return (
         <div>
-            
+            <h1>404 page</h1>
         </div>
     );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,11 +2,15 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../Pages/Home/Home";
 import Blog from "../Components/Blog/Blog";
+import Registration from "../Pages/Registration/Registration";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import SignInPage from "../Pages/login/SignInPage";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Main/>,
+        element: <Main />,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: '/',
@@ -15,6 +19,14 @@ const router = createBrowserRouter([
             {
                 path: '/blog',
                 element: <Blog/>
+            },
+            {
+                path: '/login',
+                element: <SignInPage/>
+            },
+            {
+                path: '/signup',
+                element: <Registration/>
             }
 
         ]
